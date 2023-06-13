ffe1aa6ad965b7e5d9db26fcb98db85ca06590eb
Marcos <king.frontweb@outlook.com>
2023-06-13 13:52:02 -0300

    Att: pasta projeto final

---
+++ 07_projeto_final_dogs/src/api.js
@@ -0,0 +1,54 @@
+export const API_URL = 'https://dogsapi.origamid.dev/json';
+
+export function TOKEN_POST(body) {
+  // Sistema de login, adiquire o token, se o usuario e senha ser valido
+  return {
+    url: API_URL + '/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function TOKEN_VALIDATE_POST(token) {
+  // Valida o token passado
+  return {
+    url: API_URL + '/jwt-auth/v1/token/validate',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
+
+export function USER_GET(token) {
+  // Retorna os dados do usuario por meio do token
+  return {
+    url: API_URL + '/api/user',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
+
+export function USER_POST(body) {
+  return {
+    url: API_URL + '/api/user',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
